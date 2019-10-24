a7c2c4c2fad47341fca04dae16a8cc7b10b3979a
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-24 16:27:14 -0300

    componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar/navbar';
+import List from './Components/List/list';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className='App'>
+          <Navbar/>
+          <List />
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/List/list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css'
+
+export default class List extends React.Component {
+    render() {
+        return <p>Teste</p>
+    }
+}
